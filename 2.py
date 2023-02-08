07d3b8c3af0c758eb02c4d951ccd19779b89c28a
Jeevanbagade <124168002+Jeevanbagade@users.noreply.github.com>
2023-02-08 17:54:43 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,32 @@
+# # # # # p,q,r=5,10,2
+# # # # # print(q,r)
+# # # # # p,q=q,p
+# # # # # print(p,q)
+
+# # # # a,b,c=10,2,5
+# # # # a,b,c=a+1,b*2,c-1
+# # # # print(a,b,c)
+
+# # # x=10
+# # # y=x+1
+# # # print(id(y))
+# # # y=x/2
+# # # print(id(y))
+# # # print(y)
+
+# # x=10
+# # print(type(x))
+# # print(x)
+# # x="hello world"
+# # print(type(x))
+# # print(x)
+
+# a=10
+# ans=0/a
+# print(ans)
+
+def sqr(num):
+    res=num*num
+    print(res)
+inp=int(input("enter the number"))
+sqr(inp)
